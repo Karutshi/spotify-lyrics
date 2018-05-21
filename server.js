b73df10af9556ea485529b793029ec216a59632e
Karutshi <johan.ribom@hotmail.com>
2018-05-21 15:37:47 +0200

    :construction: Add a basic login button for spotify

--- server.js
+++ server.js
@@ -6,6 +6,31 @@ var publicRequest = function(path){
   return path.substring(0, 8) == "/public/";
 }
 
+var spotifyLogin = function(req, res){
+  var scope = 'user-read-private user-read-email';
+  res.writeHead(302, {
+    'Location': 'https://accounts.spotify.com/authorize?' + 'response_type=code&' + 'client_id=bf7432eaab5c4a158b93c911b150eb9b&' + 'show_dialog=true&' + 'redirect_uri=http://localhost:8080/callback'
+    //add other headers here...
+  });
+  res.end()
+
+  /*res.redirect('') +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));*/
+}
+
+var handleCallback = function(urldata, res){
+  var params = urldata.query
+  console.log(params['code']);
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Did it!');
+}
+
 var send404Error = function(res) {
   fs.readFile(__dirname + '/pages/goblet-not-found/index.html', function(err, data){
     res.writeHead(404, {'Content-Type': 'text/html'});
@@ -14,8 +39,7 @@ var send404Error = function(res) {
       return res.end()
     }
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
+    res.end(data);
   });
 }
 
@@ -31,9 +55,13 @@ var servePublicFile = function(path, res){
 var serverfunc = function (req, res) {
   var urldata = url.parse(req.url, true);
   var path = urldata.pathname;
-  console.log('Got request for ' + path);
+  console.log('Got a %s request for %s', req.method, path);
   if (publicRequest(path)) {
     return servePublicFile(path, res);
+  } else if (path == '/test') {
+    return spotifyLogin(req, res);
+  } else if (path == '/callback') {
+    return handleCallback(urldata, res);
   }
   fs.readFile(__dirname + '/pages/' + path + 'index.html', function(err, data){
     if (err) {
